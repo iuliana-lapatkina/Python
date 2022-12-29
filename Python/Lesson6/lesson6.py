742e627bec95fe7aff5350c6e81b1dbcc29a3ed1
Iuliana <dolganova.yuliana@yandex.ru>
2022-12-29 17:23:58 +0300

    lesson6

--- Python/Lesson6/lesson6.py
+++ Python/Lesson6/lesson6.py
@@ -3,25 +3,25 @@
 # оставшихся чисел в одну строчку через пробел.
 # пример - 8 11 0 -23 140 1 => 11 -23
 
-# string_num = input("Введите числа: ").split()
-# print(string_num)
-# string_num = filter(lambda i: len(str(abs(int(i)))) == 2, string_num)
-# print(*string_num)
+string_num = input("Введите числа: ").split()
+print(string_num)
+string_num = filter(lambda i: len(str(abs(int(i)))) == 2, string_num)
+print(*string_num)
 
 # 2. Дан список, вывести отдельно буквы и цифры.
 # a = ( "a", 'b', '2', '3' ,'c')
 # b = ( 'a' , 'b' , 'c')
 # c = ( '2', '3')
 
-# a = ( "a", 'b', '2', '3' ,'c')
-# b = ( 'a' , 'b' , 'c')
-# c = ( '2', '3')
+a = ( "a", 'b', '2', '3' ,'c')
+b = ( 'a' , 'b' , 'c')
+c = ( '2', '3')
 
-# b = filter(str.isalpha, a)
-# c = filter(str.isdigit, a)
+b = filter(str.isalpha, a)
+c = filter(str.isdigit, a)
 
-# print(*b)
-# print(*c)
+print(*b)
+print(*c)
 
 # Преобразовать набора списков users = ['user1','user2','user3','user4','user5'] ids = [4, 5, 9, 14, 7] salary = [111,222,333] в другой набор
 # ['user1', 4, 111]
